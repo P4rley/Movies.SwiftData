119476994f13427f9f4ffae3d2f5281d2e38218e
Adwitya Hersa <adwihersa@gmail.com>
2025-03-13 12:29:06 +0700

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Adwitya Hersa on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
